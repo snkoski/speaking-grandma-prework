2a8c256d0b9c946accc978092bb189028cb9da5d
Shawn Koski <shawnnkoski@gmail.com>
2018-03-29 01:08:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,5 +14,9 @@
 def speak_to_grandma(comment)
   if (comment == "I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!"
+  elsif (comment == comment.upcase)
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
